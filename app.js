abab82ecf73c8de6e035e5cb83aebfb5678e04ea
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-05-30 12:29:22 +0530

    javascript added

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('openButton');
+    button.addEventListener('click', () => {
+        window.open('https://www.dell.com/en-in/shop/scc/sr/laptops/inspiron-laptops', '_blank');
+    });
+});
